fc3ad702831982b71551187747ba73839b3ba7c7
mukumbasar <mehmetutkununoyunlari@gmail.com>
2024-11-24 19:20:26 +0300

    fix: Finalize "TestSearchBarAndArticleNavigation" method by adding necessary Assert conditions.

--- Sites/2NHaber/Components/SearchBar/SearchBarTests.cs
+++ Sites/2NHaber/Components/SearchBar/SearchBarTests.cs
@@ -13,42 +13,45 @@ namespace _2NTech.Demo.Tests.Sites._2NHaber.Components.SearchBar
     public class SearchTests : BaseTest
     {
         /// <summary>
-        /// Searches for a keyword and navigates to the details of the specified news item on the search results page.
+        /// Searches for a keyword and navigates to the details of the selected article.
         /// </summary>
         /// <param name="newsIndex">The position of the news article to open through search bar results.</param>
-        [TestCase(6)] // Example input for the 8th article
-        //[TestCase(3)] // Example input for the 3rd article
+        [TestCase(8)] // Test for the 8th article
         public void TestSearchBarAndArticleNavigation(int articleIndex)
         {
             // Initialize WebDriverWait with a timeout
             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
 
-            // Wait for the search button and click it
-            var searchButton = wait.Until(driver => driver.FindElement(By.XPath("//*[@id=\"cmsmasters-scroll-top\"]/div/div/div/section/div/div[3]/div/div[2]/div/div/div[2]/div"))); // Replace with the actual class or locator for the search button
+            // Wait and check for the search button and click it
+            var searchButton = wait.Until(driver => driver.FindElement(By.XPath("//*[@id=\"cmsmasters-scroll-top\"]/div/div/div/section/div/div[3]/div/div[2]/div/div/div[2]/div")));
+            Assert.IsNotNull(searchButton, "The search button does not exist.");
             searchButton.Click();
 
+            // Wait and check for the search bar button and submit "İstanbul" with it
             var searchBar = wait.Until(driver => driver.FindElement(By.XPath("//*[@id=\"cmsmasters-scroll-top\"]/div/div/div/section/div/div[3]/div/div[2]/div/div/div[1]/div/form/div/input")));
+            Assert.IsNotNull(searchBar, "The search bar does not exist.");
             searchBar.SendKeys("İstanbul");
             searchBar.Submit();
 
             //// Wait for the search results container to load and obtain articles
-            var resultsContainer = wait.Until(driver => driver.FindElement(By.XPath("//*[@id=\"cmsmasters_body\"]/div[2]/div/div/section[2]/div/div[1]/div/div/div/div/div[2]/div/div")));
+            var articles = wait.Until(driver => driver.FindElements(By.XPath("//*[@id=\"cmsmasters_body\"]/div[2]/div/div/section[2]/div/div[1]/div/div/div/div/div[2]/div/div//article")));
+            Assert.IsTrue(articles.Count > 0, "No articles were found.");
 
-            var articles = resultsContainer.FindElements(By.TagName("article"));
-
-            // Obtain selectedArticle using articleIndex and start and check for navigation
+            // Obtain and check article anchor and href value from selectedArticle using articleIndex
+            Assert.IsTrue(articleIndex > 0 && articleIndex <= articles.Count, "The selected article does not exist.");
             var selectedArticle = articles[articleIndex - 1];
-            var articleLink = selectedArticle.FindElement(By.TagName("a"));
+            var articleAnchor = selectedArticle.FindElement(By.TagName("a"));
+            string hrefValue = articleAnchor.GetAttribute("href");
+            Assert.IsNotNull(hrefValue, "The hrefValue does not exist.");
 
+            // Instance a jsExecutor, scroll into view of the anchor and click to start navigation
             var jsExecutor = (IJavaScriptExecutor)_driver;
-            jsExecutor.ExecuteScript("arguments[0].scrollIntoView(true);", articleLink);
-            wait.Until(driver => articleLink.Displayed && articleLink.Enabled);
-
-            string hrefValue = articleLink.GetAttribute("href");
-            jsExecutor.ExecuteScript("arguments[0].click();", articleLink);
+            jsExecutor.ExecuteScript("arguments[0].scrollIntoView(true);", articleAnchor);
+            wait.Until(driver => articleAnchor.Displayed && articleAnchor.Enabled);
+            jsExecutor.ExecuteScript("arguments[0].click();", articleAnchor);
 
             // Check if the driver URL matches the href value
-            wait.Until(driver => driver.Url == hrefValue);
+            Assert.AreEqual(hrefValue, wait.Until(driver => driver.Url), "The navigation did not reach the expected URL.");
         }
     }
 }
